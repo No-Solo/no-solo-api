39333bc0cd333de141aebf0b6aa4506c3156a38a
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-08-14 22:26:57 +0300

    packages #8: setup EF/Postgre & connected to project

---
+++ NoSoloAPI/Infrastructure/Data/DataBaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class DataBaseContext : DbContext
+{
+    public DataBaseContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
