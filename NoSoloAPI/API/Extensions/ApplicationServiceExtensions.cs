434104b13029de51ca73cb8303b1cb7726348cda
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-08-15 21:42:51 +0300

    feature #23: added a basic application service extensions (added cors)

---
+++ NoSoloAPI/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddAplicationService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+        });
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDbContext<DataBaseContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnectionString"));
+        });
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}
