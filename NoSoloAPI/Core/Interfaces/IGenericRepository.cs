739fb86e0168751f3dee60d072c38d91e825e6c5
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-08-18 23:53:16 +0300

    feature #9: Implemented a Repository pattern

---
+++ NoSoloAPI/Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetByGuidAsync(Guid id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    // Task<T> GetEntityWithSpec(ISpecification<T> spec);
+    // Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    // Task<int> CountAsync(ISpecification<T> spec);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ NoSoloAPI/Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly DataBaseContext _dataBaseContext;
+
+    public GenericRepository(DataBaseContext dataBaseContext)
+    {
+        _dataBaseContext = dataBaseContext;
+    }
+    
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dataBaseContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T> GetByGuidAsync(Guid id)
+    {
+        return await _dataBaseContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        return await _dataBaseContext.Set<T>().ToListAsync();
+    }
+
+    public void Add(T entity)
+    {
+        _dataBaseContext.Set<T>().Add(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _dataBaseContext.Set<T>().Attach(entity);
+        _dataBaseContext.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Delete(T entity)
+    {
+        _dataBaseContext.Set<T>().Remove(entity);
+    }
+}
