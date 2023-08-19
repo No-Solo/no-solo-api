14e15b626f55d0d70187f08bb11a56afa44e8773
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-08-19 12:56:35 +0300

    feature #10: implemented a unit of work pattern

---
+++ NoSoloAPI/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+
+namespace Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DataBaseContext _dataBaseContext;
+
+    private Hashtable _repositories;
+
+    public UnitOfWork(DataBaseContext dataBaseContext)
+    {
+        _dataBaseContext = dataBaseContext;
+    }
+    
+    public IUserRepository UserRepository => new UserRepository();
+    public IOrganizationRepository OrganizationRepository => new OrganizationRepository();
+    
+    public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+    {
+        if(_repositories == null) 
+            _repositories = new Hashtable();
+    
+        var type = typeof(TEntity).Name;
+    
+        if (!_repositories.ContainsKey(type))
+        {
+            var repositoryType = typeof(GenericRepository<>);
+            var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _dataBaseContext);
+    
+            _repositories.Add(type, repositoryInstance);
+        }
+    
+        return (IGenericRepository<TEntity>) _repositories[type];
+    }
+
+    public async Task<bool> Complete()
+    {
+        return await _dataBaseContext.SaveChangesAsync() > 0;
+    }
+
+    public bool HasChanges()
+    {
+        return _dataBaseContext.ChangeTracker.HasChanges();
+    }
+
+    public void Dispose()
+    {
+        _dataBaseContext.Dispose();
+    }
+}
