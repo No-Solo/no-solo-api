7d28d8390f9dbbf8368fe8924a0db6a6790302f4
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-08-19 12:56:35 +0300

    feature #10: implemented a unit of work pattern

---
+++ NoSoloAPI/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository UserRepository { get; }
+    IOrganizationRepository OrganizationRepository { get; }
+    IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+    Task<bool> Complete();
+    bool HasChanges();
+    void Dispose();
+}
