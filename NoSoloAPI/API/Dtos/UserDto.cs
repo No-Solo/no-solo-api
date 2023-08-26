76c1ac1fef3afbe4c6b708886d064d417e0a8970
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-08-26 16:36:02 +0300

    feature #39: setup basic dtos (userdto, basedto, login/register dtos)

---
+++ NoSoloAPI/API/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Dtos;
+
+public class UserDto
+{
+    public string Login { get; set; }
+    
+    public string Email { get; set; }
+
+    // public List<OrganizationUserDto> OrganizationUsers { get; set; }
+    //
+    // public UserProfileDto UserProfile { get; set; }
+}
