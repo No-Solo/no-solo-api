72d7de72a68c7fca5ac898077deb879a2435804d
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-08-30 22:41:52 +0300

    44 create user profile crud (#46)
    * Mapping user profile to dto & dto to user profile
    
    * added claims principle extensions
    
    * deleted photo url from photo profile
    
    * updated UserDto (UserProfile to UserProfileDto)
    
    * feature #44: created user profile crud (without delete)

--- NoSoloAPI/Core/Entities/UserProfile.cs
+++ NoSoloAPI/Core/Entities/UserProfile.cs
@@ -11,8 +11,7 @@ public class UserProfile : BaseEntity
     public string About { get; set; }
     public string Description { get; set; }
     public string Location { get; set; }
-
-    public string PhotoUrl { get; set; }
+    
     public UserPhoto Photo { get; set; }
 
     public LocaleEnum Locale { get; set; }
