53f9391cc3e745a32eaa9236cf95b7318e52bd77
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-10 01:07:05 +0300

    feature #58: updated user profile controller for using specification

---
+++ NoSoloAPI/Core/Specification/BaseSpecification/BasicParams.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Specification;
+
+public class BasicParams
+{
+    private const int MaxPageSize = 50;
+    private int _pageSize = 9;
+    public int PageNumber { get; set; } = 1;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
