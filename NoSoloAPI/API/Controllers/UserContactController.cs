909ad8868e877b5c545172553e534c376b32f04b
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-10 23:50:17 +0300

    feature #67: updated the user contact controller to use the specification pattern

--- NoSoloAPI/API/Controllers/UserContactController.cs
+++ NoSoloAPI/API/Controllers/UserContactController.cs
@@ -1,9 +1,11 @@
 ﻿using API.Dtos;
 using API.Errors;
 using API.Extensions;
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specification.UserContact;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,7 +23,18 @@ public class UserContactController : BaseApiController
         _mapper = mapper;
     }
 
-    [HttpGet("contacts", Name = "GetUserContacts")]
+    [HttpGet("my/contacts/with-params", Name = "GetUserContactsWithParams")]
+    public async Task<ActionResult<Pagination<ContactDto>>> GetUserProfileContacts([FromQuery] UserContactParams userContactParams)
+    {
+        var userProfile = await
+            _unitOfWork.UserProfileRepository.GetUserProfileByUsernameWithContactsIncludeAsync(User.GetUsername());
+
+        userContactParams.UserProfileId = userProfile.Id;
+        
+        return Ok(await GetUserContactsBySpecificationParams(userContactParams));
+    }
+    
+    [HttpGet("my/contacts", Name = "GetUserContacts")]
     public async Task<ActionResult<IReadOnlyList<ContactDto>>> GetUserProfileContacts()
     {
         var userProfile = await
@@ -30,23 +43,23 @@ public class UserContactController : BaseApiController
         if (userProfile.Contacts == null)
             return NotFound(new ApiResponse(404, "The contacts not found"));
         
-        var userProfileToReturn = _mapper.Map<UserProfileDto>(userProfile);
-        
-        return Ok(_mapper.Map<IReadOnlyList<ContactDto>>(userProfileToReturn.Contacts));
+        return Ok(_mapper.Map<IReadOnlyList<ContactDto>>(userProfile.Contacts));
     }
     
-    // [HttpGet("my/{id:guid}")]
-    // public async Task<ActionResult<ContactDto>> GetMyUserProfileContactByGuid(Guid id)
-    // {
-    //     
-    // }
-    //
-    // [HttpGet("{id:guid}")]
-    // public async Task<ActionResult<ContactDto>> GetUserProfileContactByGuid(Guid id)
-    // {
-    //     var userProfile = await _unitOfWork.UserProfileRepository.GetUserProfileByUsernameWithContactsIncludeAsync(id);
-    // }
-    
+    [HttpGet("my/{id:guid}")]
+    public async Task<ActionResult<ContactDto>> GetMyUserProfileContactByGuid(Guid id)
+    {
+        var userProfile = await
+            _unitOfWork.UserProfileRepository.GetUserProfileByUsernameWithContactsIncludeAsync(User.GetUsername());
+
+        var contact = userProfile.Contacts.SingleOrDefault(x => x.Id == id);
+
+        if (contact == null)
+            return NotFound(new ApiResponse(404, "The contact not found"));
+
+        return Ok(_mapper.Map<ContactDto>(contact));
+    }
+
     [HttpPut("update")]
     public async Task<ActionResult> UpdateUserProfileContact(ContactDto contactDto)
     {
@@ -109,5 +122,21 @@ public class UserContactController : BaseApiController
             return Ok();
         
         return BadRequest(new ApiResponse(400, "Failed to delete contact from your profile"));
+    }
+    
+    private async Task<Pagination<ContactDto>> GetUserContactsBySpecificationParams(UserContactParams userContactParams)
+    {
+        var spec = new UserContactWithSpecificationParams(userContactParams);
+        
+        var countSpec = new UserContactWithFiltersForCountSpecification(userContactParams);
+
+        var totalItems = await _unitOfWork.Repository<Contact<UserProfile>>().CountAsync(countSpec);
+        
+        var userOffers = await _unitOfWork.Repository<Contact<UserProfile>>().ListAsync(spec);
+        
+        var data = _mapper
+            .Map<IReadOnlyList<Contact<UserProfile>>, IReadOnlyList<ContactDto>>(userOffers);
+
+        return new Pagination<ContactDto>(userContactParams.PageNumber, userContactParams.PageSize, totalItems, data);
     }
 }
