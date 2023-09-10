06d741af259eab2a8e7b9235c3c4db3aed550f80
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-10 21:27:28 +0300

    feature/migration #60: updated the user offer controller to use the specification pattern

---
+++ NoSoloAPI/Core/Specification/UserOffer/UserOfferParams.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Specification.UserOffer;
+
+public class UserOfferParams : BasicParams
+{
+    public Guid? UserProfileId { get; set; }
+    public string? SortByAlphabetical { get; set; }
+    public string? SortByDate { get; set; }
+    
+    private string _search;
+    public string? Search
+    { 
+        get => _search;
+        set => _search = value.ToLower();
+    }
+}
