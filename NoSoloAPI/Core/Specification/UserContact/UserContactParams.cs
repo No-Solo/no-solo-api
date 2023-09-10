24b9e9654963a05254c647617281436fa1f24646
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-10 23:50:17 +0300

    feature #67: updated the user contact controller to use the specification pattern

---
+++ NoSoloAPI/Core/Specification/UserContact/UserContactParams.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Specification.UserContact;
+
+public class UserContactParams : BasicParams
+{
+    public Guid? UserProfileId { get; set; }
+    
+    public string? SortByAlphabetical { get; set; }
+    private string _search;
+    public string? Search
+    { 
+        get => _search;
+        set => _search = value.ToLower();
+    }
+}
