4e70ec1e335d2a4702cf8bbd0ca89b40bea04e33
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-10 22:55:47 +0300

    feature #61: updated the user tag controller to use the specification pattern

---
+++ NoSoloAPI/Core/Specification/UserTag/UserTagWithFiltersForCountSpecification.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Specification.UserTag;
+
+public class UserTagWithFiltersForCountSpecification : BaseSpecification<Entities.UserTag>
+{
+    public UserTagWithFiltersForCountSpecification(UserTagParams userTagParams)
+        : base(x => (string.IsNullOrEmpty(userTagParams.Search) || x.Description.ToLower().Contains(userTagParams.Search))
+                    && (!userTagParams.UserProfileId.HasValue || x.UserProfileId == userTagParams.UserProfileId))
+    {
+        
+    }
+}
