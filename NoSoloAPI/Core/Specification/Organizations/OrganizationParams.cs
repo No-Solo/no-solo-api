24010b0847f1ac119299d192f81dad9ce33b49d2
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-11 20:28:01 +0300

    feature #13: created organization crud

---
+++ NoSoloAPI/Core/Specification/Organizations/OrganizationParams.cs
@@ -0,0 +1,21 @@
+﻿namespace Core.Specification.Organizations;
+
+public class OrganizationParams : BasicParams
+{
+    private int _pageSize = 6;
+    
+    public bool WithOffers { get; set; }
+    public bool WithPhotos { get; set; }
+    public bool WithContacts { get; set; }
+    public bool WithMembers { get; set; }
+    
+    public string? SortByAlphabetical { get; set; }
+    public string? SortByDate { get; set; }
+    
+    private string _search;
+    public string? Search
+    { 
+        get => _search;
+        set => _search = value.ToLower();
+    }
+}
---
+++ NoSoloAPI/Core/Specification/Organizations/OrganizationWithFiltersForCountSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Core.Specification.Organizations;
+
+public class OrganizationWithFiltersForCountSpecification : BaseSpecification<Organization>
+{
+    public OrganizationWithFiltersForCountSpecification(OrganizationParams organizationParams)
+        : base(x => string.IsNullOrEmpty(organizationParams.Search) || x.Name.Contains(organizationParams.Search))
+    {
+        
+    }
+}
