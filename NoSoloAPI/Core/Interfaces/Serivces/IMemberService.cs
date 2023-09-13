a5de678352754b97515d5ba0a636022f7cf28732
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-13 20:18:59 +0300

    created member service

---
+++ NoSoloAPI/Core/Interfaces/Serivces/IMemberService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Core.Enums;
+
+namespace Core.Interfaces.Serivces;
+
+public interface IMemberService
+{
+    Task<OrganizationUser> MoreAsync(OrganizationUser member1, OrganizationUser member2);
+    Task<OrganizationUser> LessAsync(OrganizationUser member1, OrganizationUser member2);
+    bool More(OrganizationUser member1, OrganizationUser member2);
+    bool Less(OrganizationUser member1, OrganizationUser member2);
+
+    RoleEnum ParseRole(string role);
+    
+    Task<RoleEnum> GetMemberRoleAsync(Organization organization, User user);
+    Task<RoleEnum> GetMemberRoleAsync(Organization organization, string username);
+    
+    Task<bool> MemberHasRoleAsync(RoleEnum role, Organization organization, User user);
+    Task<bool> MemberHasRoleAsync(RoleEnum role, Organization organization, string username);
+    Task<bool> MemberHasRolesAsync(IEnumerable<RoleEnum> roles, Organization organization, User user);
+    Task<bool> MemberHasRolesAsync(IEnumerable<RoleEnum> roles, Organization organization, string username);
+}
