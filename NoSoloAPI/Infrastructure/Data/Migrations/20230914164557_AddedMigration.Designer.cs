37cf6065542e86370b7dccec0a4229bb129bef64
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-14 19:52:44 +0300

    added migration

--- NoSoloAPI/Infrastructure/Data/Migrations/20230914164557_AddedMigration.Designer.cs
+++ NoSoloAPI/Infrastructure/Data/Migrations/20230914164557_AddedMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230914164557_AddedMigration")]
+    partial class AddedMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
