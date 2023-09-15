c9ad4aba207d4728ce0556b43e7f311c32e4b7b2
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-15 15:01:13 +0300

    feature #81: created the user config

---
+++ NoSoloAPI/Infrastructure/Data/Config/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(p => p.Id).IsRequired();
+        builder.Property(x => x.UserName).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.PhoneNumber).HasMaxLength(10);
+    }
+}
