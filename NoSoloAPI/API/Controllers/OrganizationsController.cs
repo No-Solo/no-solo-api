170223b28338f93a055b644eb2a01568932de02f
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-09-15 22:51:59 +0300

    feature #87: refactored the organization controller

--- NoSoloAPI/API/Controllers/OrganizationsController.cs
+++ NoSoloAPI/API/Controllers/OrganizationsController.cs
@@ -14,6 +14,7 @@ using Core.Specification.Organization.OrganizationOffer;
 using Core.Specification.Organization.OrganizationPhotoParams;
 using Core.Specification.OrganizationContact;
 using Core.Specification.Organizations;
+using Core.Specification.Project;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -34,6 +35,8 @@ public class OrganizationsController : BaseApiController
         _memberService = memberService;
         _photoService = photoService;
     }
+
+    #region Organizaiton
 
     [AllowAnonymous]
     [HttpGet("all/with-params")]
@@ -213,12 +216,11 @@ public class OrganizationsController : BaseApiController
 
         return BadRequest(new ApiResponse(400, "Failed to upgrade role"));
     }
+    
+    #endregion
 
-    /// <summary>
-    /// Contacts
-    /// </summary>
-    /// <param name="organizationId"></param>
-    /// <returns></returns>
+    #region Organization Contacts
+
     [AllowAnonymous]
     [HttpGet("{organizationId:guid}/contacts")]
     public async Task<ActionResult<IReadOnlyList<ContactDto>>> GetOrganizationContacts(Guid organizationId)
@@ -327,81 +329,10 @@ public class OrganizationsController : BaseApiController
         return BadRequest(new ApiResponse(400, "Failed to delete contact from your profile"));
     }
 
-    private async Task<Pagination<OrganizationDto>> GetOrganizationsBySpecificationParams(
-        OrganizationParams organizationParams)
-    {
-        var spec = new OrganizationWithSpecificationParams(organizationParams);
+    #endregion
 
-        var countSpec = new OrganizationWithFiltersForCountSpecification(organizationParams);
+    #region Organization Offers
 
-        var totalItems = await _unitOfWork.Repository<Organization>().CountAsync(countSpec);
-
-        var organizations = await _unitOfWork.Repository<Organization>().ListAsync(spec);
-
-        var data = _mapper
-            .Map<IReadOnlyList<Organization>, IReadOnlyList<OrganizationDto>>(organizations);
-
-        return new Pagination<OrganizationDto>(organizationParams.PageNumber, organizationParams.PageSize, totalItems,
-            data);
-    }
-
-    private async Task<Pagination<ContactDto>> GetOrganizationContactsBySpecificationParams(
-        OrganizationContactParams organizationContactParams)
-    {
-        var spec = new OrganizationContactWithSpecificationParams(organizationContactParams);
-
-        var countSpec = new OrganizationContactWithFiltersForCountSpecification(organizationContactParams);
-
-        var totalItems = await _unitOfWork.Repository<Contact<Organization>>().CountAsync(countSpec);
-
-        var userOffers = await _unitOfWork.Repository<Contact<Organization>>().ListAsync(spec);
-
-        var data = _mapper
-            .Map<IReadOnlyList<Contact<Organization>>, IReadOnlyList<ContactDto>>(userOffers);
-
-        return new Pagination<ContactDto>(organizationContactParams.PageNumber, organizationContactParams.PageSize,
-            totalItems, data);
-    }
-
-    private async Task<Pagination<OrganizationOfferDto>> GetOrganizationOffersBySpecificationParams(
-        OrganizationOfferParams organizationOfferParams)
-    {
-        var spec = new OrganizationOfferWithSpecificationParams(organizationOfferParams);
-
-        var countSpec = new OrganizationOfferWithFiltersForCountSpecification(organizationOfferParams);
-
-        var totalItems = await _unitOfWork.Repository<OrganizationOffer>().CountAsync(countSpec);
-
-        var userOffers = await _unitOfWork.Repository<OrganizationOffer>().ListAsync(spec);
-
-        var data = _mapper
-            .Map<IReadOnlyList<OrganizationOffer>, IReadOnlyList<OrganizationOfferDto>>(userOffers);
-
-        return new Pagination<OrganizationOfferDto>(organizationOfferParams.PageNumber,
-            organizationOfferParams.PageSize, totalItems, data);
-    }
-
-    private async Task<Pagination<OrganizationPhotoDto>> GetOrganizationPhotosBySpecificationParams(
-        OrganizationPhotoParams organizationPhotoParams)
-    {
-        var spec = new OrganizationPhotoWithSpecificationParams(organizationPhotoParams);
-
-        var countSpec = new OrganizationPhotoWithPaginationForCountSpecification(organizationPhotoParams);
-
-        var totalItems = await _unitOfWork.Repository<OrganizationPhoto>().CountAsync(countSpec);
-
-        var userOffers = await _unitOfWork.Repository<OrganizationPhoto>().ListAsync(spec);
-
-        var data = _mapper
-            .Map<IReadOnlyList<OrganizationPhoto>, IReadOnlyList<OrganizationPhotoDto>>(userOffers);
-
-        return new Pagination<OrganizationPhotoDto>(organizationPhotoParams.PageNumber,
-            organizationPhotoParams.PageSize, totalItems, data);
-    }
-
-    /// <summary>
-    /// Organization Offers
-    /// </summary>
     [AllowAnonymous]
     [HttpGet("{organizationId:guid}/offers/with-params")]
     public async Task<ActionResult<Pagination<OrganizationOfferDto>>> GetOrganizationOffersWithParams(
@@ -526,9 +457,10 @@ public class OrganizationsController : BaseApiController
         return BadRequest(new ApiResponse(400, "Failed to create offer"));
     }
 
-    /// <summary>
-    /// Organization Offers
-    /// </summary>
+    #endregion
+
+    #region Organization Photo
+
     [AllowAnonymous]
     [HttpGet("{organizationId:guid}/photo")]
     public async Task<ActionResult<OrganizationPhotoDto>> GetMainOrganizationPhoto(Guid organizationId)
@@ -619,7 +551,7 @@ public class OrganizationsController : BaseApiController
         if (photo.IsMain)
             return BadRequest("This is already your main photo");
         
-        var currentMainPhoto = organization.Photos.FirstOrDefault(photo => photo.IsMain);
+        var currentMainPhoto = organization.Photos.FirstOrDefault(organizationPhoto => organizationPhoto.IsMain);
         
         if (currentMainPhoto != null)
             currentMainPhoto.IsMain = false;
@@ -667,4 +599,207 @@ public class OrganizationsController : BaseApiController
 
         return BadRequest("Problem deleting photo");
     }
+
+    #endregion
+
+    #region Project
+
+    [HttpGet("project/{id:guid}")]
+    public async Task<ActionResult<ProjectDto>> GetProjectByGuid(Guid id)
+    {
+        var project = await _unitOfWork.Repository<Project>().GetByGuidAsync(id);
+
+        if (project == null)
+            return NotFound(new ApiResponse(404, "The project not found"));
+
+        return Ok(_mapper.Map<ProjectDto>(project));
+    }
+
+    [HttpGet("projects")]
+    public async Task<ActionResult<Pagination<ProjectDto>>> GetAllProjects([FromQuery] ProjectParams projectParams)
+    {
+        return Ok(await GetProjectsBySpecificationParams(projectParams));
+    }
+
+    [HttpPost("{organizationId:guid}/project/create")]
+    public async Task<ActionResult<ProjectDto>> CreateProject(Guid id, [FromBody] CreateProjectDto createProjectDto)
+    {
+        var user = await _unitOfWork.UserRepository.GetUserByUsernameWithMembersIncludeAsync(User.GetUsername());
+
+        var organization = await _unitOfWork.OrganizationRepository.GetOrganizationWithProjectIncludeByGuid(id);
+
+        if (organization.Project != null)
+            return BadRequest(new ApiResponse(400, "The project is exist"));
+
+        var member = user.OrganizationUsers.SingleOrDefault(x => x.OrganizationId == organization.Id && x.UserId == user.Id);
+
+        if (member == null)
+            return NotFound(new ApiResponse(404, "The member's instance not found"));
+        
+        if (member.Role != RoleEnum.Administrator || member.Role != RoleEnum.Owner)
+            return BadRequest(new ApiResponse(400, "You don't have permissions"));
+
+        createProjectDto.Name ??= organization.Name;
+
+        var project = new Project
+        {
+            Name = createProjectDto.Name,
+            Description = createProjectDto.Description
+        };
+
+        organization.Project = project;
+
+        if (await _unitOfWork.Complete())
+            return Ok(_mapper.Map<ProjectDto>(project));
+        
+        return BadRequest(new ApiResponse(400, "Failed to create organization's project"));
+    }
+
+    [HttpPut("{organizationId:guid}/project/update")]
+    public async Task<ActionResult<ProjectDto>> UpdateProject(Guid organizationId, [FromBody] ProjectDto projectDto)
+    {
+        var user = await _unitOfWork.UserRepository.GetUserByUsernameWithMembersIncludeAsync(User.GetUsername());
+        
+        var organization = await _unitOfWork.OrganizationRepository.GetOrganizationWithProjectIncludeByGuid(organizationId);
+
+        if (organization.Project != null)
+            return BadRequest(new ApiResponse(400, "The project is exist"));
+
+        var member = user.OrganizationUsers.SingleOrDefault(x => x.OrganizationId == organization.Id && x.UserId == user.Id);
+
+        if (member == null)
+            return NotFound(new ApiResponse(404, "The member's instance not found"));
+        
+        if (member.Role != RoleEnum.Owner || member.Role != RoleEnum.Administrator || member.Role != RoleEnum.Moderator)
+            return BadRequest(new ApiResponse(400, "You don't have permissions"));
+
+        var project = await _unitOfWork.Repository<Project>().GetByGuidAsync(organizationId);
+
+        _mapper.Map(projectDto, project);
+        
+        if (await _unitOfWork.Complete())
+            return Ok();
+
+        return BadRequest(new ApiResponse(400, "Failed to delete project"));
+    }
+
+    [HttpDelete("{organizationId:guid}/project/delete")]
+    public async Task<ActionResult> DeleteProject(Guid organizationId)
+    {
+        var user = await _unitOfWork.UserRepository.GetUserByUsernameWithMembersIncludeAsync(User.GetUsername());
+        
+        var organization = await _unitOfWork.OrganizationRepository.GetOrganizationWithProjectIncludeByGuid(organizationId);
+
+        if (organization.Project != null)
+            return BadRequest(new ApiResponse(400, "The project is exist"));
+
+        var member = user.OrganizationUsers.SingleOrDefault(x => x.OrganizationId == organization.Id && x.UserId == user.Id);
+
+        if (member == null)
+            return NotFound(new ApiResponse(404, "The member's instance not found"));
+        
+        if (member.Role != RoleEnum.Owner)
+            return BadRequest(new ApiResponse(400, "You don't have permissions"));
+
+        organization.Project = null;
+
+        if (await _unitOfWork.Complete())
+            return Ok();
+
+        return BadRequest(new ApiResponse(400, "Failed to delete project"));
+    }
+
+    #endregion
+    
+    #region Specification
+
+    private async Task<Pagination<OrganizationDto>> GetOrganizationsBySpecificationParams(
+        OrganizationParams organizationParams)
+    {
+        var spec = new OrganizationWithSpecificationParams(organizationParams);
+
+        var countSpec = new OrganizationWithFiltersForCountSpecification(organizationParams);
+
+        var totalItems = await _unitOfWork.Repository<Organization>().CountAsync(countSpec);
+
+        var organizations = await _unitOfWork.Repository<Organization>().ListAsync(spec);
+
+        var data = _mapper
+            .Map<IReadOnlyList<Organization>, IReadOnlyList<OrganizationDto>>(organizations);
+
+        return new Pagination<OrganizationDto>(organizationParams.PageNumber, organizationParams.PageSize, totalItems,
+            data);
+    }
+
+    private async Task<Pagination<ContactDto>> GetOrganizationContactsBySpecificationParams(
+        OrganizationContactParams organizationContactParams)
+    {
+        var spec = new OrganizationContactWithSpecificationParams(organizationContactParams);
+
+        var countSpec = new OrganizationContactWithFiltersForCountSpecification(organizationContactParams);
+
+        var totalItems = await _unitOfWork.Repository<Contact<Organization>>().CountAsync(countSpec);
+
+        var userOffers = await _unitOfWork.Repository<Contact<Organization>>().ListAsync(spec);
+
+        var data = _mapper
+            .Map<IReadOnlyList<Contact<Organization>>, IReadOnlyList<ContactDto>>(userOffers);
+
+        return new Pagination<ContactDto>(organizationContactParams.PageNumber, organizationContactParams.PageSize,
+            totalItems, data);
+    }
+
+    private async Task<Pagination<OrganizationOfferDto>> GetOrganizationOffersBySpecificationParams(
+        OrganizationOfferParams organizationOfferParams)
+    {
+        var spec = new OrganizationOfferWithSpecificationParams(organizationOfferParams);
+
+        var countSpec = new OrganizationOfferWithFiltersForCountSpecification(organizationOfferParams);
+
+        var totalItems = await _unitOfWork.Repository<OrganizationOffer>().CountAsync(countSpec);
+
+        var userOffers = await _unitOfWork.Repository<OrganizationOffer>().ListAsync(spec);
+
+        var data = _mapper
+            .Map<IReadOnlyList<OrganizationOffer>, IReadOnlyList<OrganizationOfferDto>>(userOffers);
+
+        return new Pagination<OrganizationOfferDto>(organizationOfferParams.PageNumber,
+            organizationOfferParams.PageSize, totalItems, data);
+    }
+
+    private async Task<Pagination<OrganizationPhotoDto>> GetOrganizationPhotosBySpecificationParams(
+        OrganizationPhotoParams organizationPhotoParams)
+    {
+        var spec = new OrganizationPhotoWithSpecificationParams(organizationPhotoParams);
+
+        var countSpec = new OrganizationPhotoWithPaginationForCountSpecification(organizationPhotoParams);
+
+        var totalItems = await _unitOfWork.Repository<OrganizationPhoto>().CountAsync(countSpec);
+
+        var userOffers = await _unitOfWork.Repository<OrganizationPhoto>().ListAsync(spec);
+
+        var data = _mapper
+            .Map<IReadOnlyList<OrganizationPhoto>, IReadOnlyList<OrganizationPhotoDto>>(userOffers);
+
+        return new Pagination<OrganizationPhotoDto>(organizationPhotoParams.PageNumber,
+            organizationPhotoParams.PageSize, totalItems, data);
+    }
+
+    private async Task<Pagination<ProjectDto>> GetProjectsBySpecificationParams(ProjectParams projectParams)
+    {
+        var spec = new ProjectWithSpecificationParams(projectParams);
+        
+        var countSpec = new ProjectWithFiltersForCountSpecification(projectParams);
+
+        var totalItems = await _unitOfWork.Repository<Project>().CountAsync(countSpec);
+        
+        var projects = await _unitOfWork.Repository<Project>().ListAsync(spec);
+        
+        var data = _mapper
+            .Map<IReadOnlyList<Project>, IReadOnlyList<ProjectDto>>(projects);
+
+        return new Pagination<ProjectDto>(projectParams.PageNumber, projectParams.PageSize, totalItems, data);
+    }
+    
+    #endregion
 }
