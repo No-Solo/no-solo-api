2e99ad4fe490d42f5566dd4c3238bf9d9ef88a39
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-08 00:38:33 +0200

    refactor #126: created the scope service extensions

---
+++ NoSoloAPI/API/Extensions/ScopeServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using API.Authorization;
+using Core.Interfaces;
+using Core.Interfaces.Data;
+using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+using Infrastructure;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Extensions;
+
+public  static class ScopeServiceExtensions
+{
+    public static IServiceCollection AddScopeService(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IOrganizationRepository, OrganizationRepository>();
+
+
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPhotoService, PhotoService>();
+        services.AddScoped<IMemberService, MemberService>();
+        services.AddScoped<IRecommendService, RecommendService>();
+
+        services.AddScoped<IResponseCacheService, ResponseCacheService>();
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        services.AddSingleton<IAuthorizationHandler, HasProfileHandler>();
+
+        return services;
+    }
+}
