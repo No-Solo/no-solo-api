097d394871741ba5d4d1f97d92a6afdb23aa9445
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-08 00:48:49 +0200

    refactor #130: created the api behaviour service extensions

---
+++ NoSoloAPI/API/Extensions/ApiBehaviorServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Extensions;
+
+public static class ApiBehaviorServiceExtensions
+{
+    public static IServiceCollection AddApiBehaviourServices(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+        });
+        
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var errors = context.ModelState
+                    .Where(error => error.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                var errorResponse = new ApiValidationErrorResponse
+                {
+                    Errors = errors
+                };
+
+                return new BadRequestObjectResult(errorResponse);
+            };
+        });
+
+        return services;
+    }
+}
