e31f4681dc3e5e6a61d3b972b73c6464c16753bc
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-22 23:01:25 +0200

    added custom exceptions

---
+++ NoSoloAPI/NoSolo.Core/Exceptions/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿namespace NoSolo.Core.Exceptions;
+
+public class BadRequestException : BaseException.BaseException
+{
+    public override int StatusCode { get; protected set; } = 400;
+
+    public BadRequestException(string message) : base(message)
+    {
+
+    }
+
+    public BadRequestException() : this("Throw bad request")
+    {
+        
+    }
+}
---
+++ NoSoloAPI/NoSolo.Core/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,11 @@
+﻿namespace NoSolo.Core.Exceptions;
+
+public class InvalidCredentialsException : BaseException.BaseException
+{
+    public override int StatusCode { get; protected set; } = 403;
+
+    public InvalidCredentialsException(string message) : base(message)
+    {
+        
+    }
+}
