2a70ba86a537c95e044e59678a4d289839080616
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-22 23:01:42 +0200

    added user params (specification)

---
+++ NoSoloAPI/NoSolo.Core/Specification/Users/Users/UserParams.cs
@@ -0,0 +1,9 @@
+﻿namespace NoSolo.Core.Specification.Users.Users;
+
+public class UserParams
+{
+    public string? Email { get; set; }
+
+    public bool UserProfileInclude { get; set; }
+    public bool OrganizationsInclude { get; set; }
+}
