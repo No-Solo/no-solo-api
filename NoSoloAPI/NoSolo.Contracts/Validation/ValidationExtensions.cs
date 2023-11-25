6b284da2446aede463fd058480e714b25717c0f4
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-25 17:55:18 +0200

    added validation extensions for password

---
+++ NoSoloAPI/NoSolo.Contracts/Validation/ValidationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NoSolo.Contracts.Validation;
+
+public static class ValidationExtensions
+{
+    public static bool IsValidPassword(this string password)
+    {
+        return Regex.IsMatch(password, "([0-9]+[a-zA-Z]+[0-9a-zA-Z]*)|([a-zA-Z]+[0-9]+[0-9a-zA-Z]*)");
+    }
+}
