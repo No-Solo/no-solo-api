6c92e067886e98bc106d9f85cc367c72b434a3b5
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-25 18:03:29 +0200

    contact usage has been updated

---
+++ NoSoloAPI/NoSolo.Abstractions.Services/Contacts/IUserContactService.cs
@@ -0,0 +1,18 @@
+﻿using NoSolo.Abstractions.Services.Utility;
+using NoSolo.Contracts.Dtos.Base;
+using NoSolo.Contracts.Dtos.Base.Create;
+using NoSolo.Core.Specification.Users.UserContact;
+
+namespace NoSolo.Abstractions.Services.Contacts;
+
+public interface IUserContactService
+{
+    Task<ContactDto> Add(NewContactDto contactDto, string email);
+    
+    Task<Pagination<ContactDto>> Get(UserContactParams userContactParams, Guid guid);
+    Task<ContactDto> Get(Guid contactGuid, string email);
+
+    Task<ContactDto> Update(ContactDto contactDto, string email);
+    
+    Task Delete(Guid contactGuid, string email);
+}
