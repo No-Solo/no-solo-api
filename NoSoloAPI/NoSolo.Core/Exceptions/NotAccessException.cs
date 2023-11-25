44cf0370bf6495d443388bdf0b0a081cf2907c46
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-25 18:04:33 +0200

    added new exceptions (not access, photo)

---
+++ NoSoloAPI/NoSolo.Core/Exceptions/NotAccessException.cs
@@ -0,0 +1,16 @@
+﻿namespace NoSolo.Core.Exceptions;
+
+public class NotAccessException : BaseException.BaseException
+{
+    public override int StatusCode { get; protected set; } = 403;
+
+    public NotAccessException(string message) : base(message)
+    {
+        
+    }
+
+    public NotAccessException() : this("You don't have permission")
+    {
+        
+    }
+}
