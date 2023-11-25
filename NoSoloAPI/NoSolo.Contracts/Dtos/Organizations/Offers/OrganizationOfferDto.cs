ff0771fe8804749a6b697cd484049c99f9baff11
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-25 17:54:48 +0200

    updated organization offer dtos

--- NoSoloAPI/NoSolo.Contracts/Dtos/Organizations/Offers/OrganizationOfferDto.cs
+++ NoSoloAPI/NoSolo.Contracts/Dtos/Organizations/Offers/OrganizationOfferDto.cs
@@ -1,11 +1,10 @@
 ﻿using NoSolo.Contracts.Dtos.Base;
-using NoSolo.Core.Enums;
 
-namespace NoSolo.Contracts.Dtos.Organization;
+namespace NoSolo.Contracts.Dtos.Organizations.Offers;
 
 public class OrganizationOfferDto : BaseDto
 {
     public string? Name { get; set; }
     public string Description { get; set; }
-    public List<TagEnum> Tags { get; set; }
+    public List<string> Tags { get; set; }
 }
