f8e4f7cecd208a7bb47fe86ffd6f316698bedd81
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-30 02:57:49 +0200

    added feedback dto

---
+++ NoSoloAPI/NoSolo.Contracts/Dtos/FeedBack/FeedBackDto.cs
@@ -0,0 +1,11 @@
+﻿using NoSolo.Contracts.Dtos.Base;
+
+namespace NoSolo.Contracts.Dtos.FeedBack;
+
+public class FeedBackDto : BaseDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string FeedBackText { get; set; }
+}
