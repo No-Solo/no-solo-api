75ceac80035f16ae042bf8a65dd11e271adf3c7d
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-11-30 02:57:24 +0200

    added feedback functionality

---
+++ NoSoloAPI/NoSolo.Web.API/Middleware/Validation/FeedBack/NewFeedBackDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using NoSolo.Contracts.Dtos.FeedBack;
+
+namespace NoSolo.Web.API.Middleware.Validation.FeedBack;
+
+public class NewFeedBackDtoValidator : AbstractValidator<NewFeedBackDto>
+{
+    public NewFeedBackDtoValidator()
+    {
+        RuleFor(f => f.FeedBackText)
+            .MinimumLength(1)
+            .MaximumLength(500)
+            .NotEmpty();
+
+        RuleFor(e => e.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(f => f.FirstName)
+            .NotEmpty();
+
+        RuleFor(l => l.LastName)
+            .NotEmpty();
+    }
+}
