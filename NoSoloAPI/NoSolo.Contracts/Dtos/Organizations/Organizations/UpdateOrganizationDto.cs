2d1b005faf0bbae6a7d02effc425463f56d19027
Kirill Nesterenko <65315887+Sqvlen@users.noreply.github.com>
2023-12-01 00:24:24 +0200

    ogranization dtos has been updated

--- NoSoloAPI/NoSolo.Contracts/Dtos/Organizations/Organizations/UpdateOrganizationDto.cs
+++ NoSoloAPI/NoSolo.Contracts/Dtos/Organizations/Organizations/UpdateOrganizationDto.cs
@@ -1,8 +1,12 @@
 ﻿using NoSolo.Contracts.Dtos.Base;
 
-namespace NoSolo.Contracts.Dtos.Organization.Update;
+namespace NoSolo.Contracts.Dtos.Organizations.Organizations;
 
 public class UpdateOrganizationDto : BaseDto
 {
     public string Name { get; set; }
+    public string Description { get; set; }
+    public int? NumberOfEmployees { get; set; }
+    public string? Address { get; set; }
+    public string? WebSiteUrl { get; set; }
 }
