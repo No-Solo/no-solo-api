c6affb1fe0346a366bfe7013e56f166249d2fef0
Kirill Nesterenko <kirillnesterenka@gmail.com>
2024-05-08 18:34:00 +0300

    add worker project (for background queue of tasks) (#193)

---
+++ NoSoloAPI/NoSolo.Worker/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NoSolo.Worker.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddWorkerService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSqs();
+        
+        services.AddDatabase(configuration);
+        
+        return services;
+    }
+
+    public static IServiceCollection AddSqs(this IServiceCollection services)
+    {
+        
+        services.AddMassTransit(config =>
+        {
+            config.AddEntityFrameworkOutbox<AppDbContext>(o =>
+            {
+                o.DuplicateDetectionWindow = TimeSpan.FromSeconds(30);
+                o.UsePostgres();
+            });
+    
+            config.SetKebabCaseEndpointNameFormatter();
+
+            var assembly = typeof(Program).Assembly;
+    
+            config.AddConsumers(assembly);
+            config.AddActivities(assembly);
+    
+            config.UsingAmazonSqs((ctx, cfg) =>
+            {
+                cfg.Host("eu-west-2", _ => {});
+                cfg.ConfigureEndpoints(ctx);
+            });
+        });
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseNpgsql(configuration["Database:DefaultConnection"]!, builder =>
+            {
+                builder.EnableRetryOnFailure(5);
+            });
+        });
+
+        return services;
+    }
+}
