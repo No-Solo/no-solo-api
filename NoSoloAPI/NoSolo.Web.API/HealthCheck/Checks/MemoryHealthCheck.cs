d816bd72c8f53895d6d30f3645c16c95065f64ca
Sqvlen <kirillnesterenka@gmail.com>
2024-05-21 00:27:41 +0300

    implement healthcheck

---
+++ NoSoloAPI/NoSolo.Web.API/HealthCheck/Checks/MemoryHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using NoSolo.Web.API.HealthCheck.Checks.Options;
+
+namespace NoSolo.Web.API.HealthCheck.Checks;
+
+[ExcludeFromCodeCoverage]
+public class MemoryHealthCheck(IOptionsMonitor<MemoryCheckOptions> options) : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        var options1 = options.Get(context.Registration.Name);
+
+        // Include GC information in the reported diagnostics.
+        var allocated = GC.GetTotalMemory(forceFullCollection: false);
+        var data = new Dictionary<string, object>()
+        {
+            { "AllocatedBytes", allocated },
+            { "Gen0Collections", GC.CollectionCount(0) },
+            { "Gen1Collections", GC.CollectionCount(1) },
+            { "Gen2Collections", GC.CollectionCount(2) },
+        };
+        var status = (allocated < options1.Threshold) ? HealthStatus.Healthy : HealthStatus.Unhealthy;
+
+        return Task.FromResult(new HealthCheckResult(
+            status,
+            description: "Reports degraded status if allocated bytes " +
+                         $">= {options1.Threshold} bytes.",
+            exception: null,
+            data: data));
+    }
+}
---
+++ NoSoloAPI/NoSolo.Web.API/HealthCheck/Checks/RemoteHealthCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace NoSolo.Web.API.HealthCheck.Checks;
+
+[ExcludeFromCodeCoverage]
+public class RemoteHealthCheck(IHttpClientFactory httpClientFactory) : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = new())
+    {
+        using var httpClient = httpClientFactory.CreateClient();
+        var response = await httpClient.GetAsync("https://localhost:5001/api/feed-back", cancellationToken);
+        return response.IsSuccessStatusCode
+            ? HealthCheckResult.Healthy($"Remote endpoints is healthy.")
+            : HealthCheckResult.Unhealthy("Remote endpoint is unhealthy");
+    }
+}
